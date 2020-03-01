568b5891124e356b4ad72c7b5afdc1877d6cf984
Luminous-Lizard <jochemjellema@outlook.com>
2020-03-01 08:08:13 +0100

    bcrypt & salt

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-const app = express ();
-
-
+const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -21,18 +20,14 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
 });
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
-
 const User = new mongoose.model("User", userSchema);
 
-
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
@@ -40,38 +35,50 @@ app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.post("/login", function(req, res){
+app.post("/login", function(req, res) {
+
   const email = req.body.username;
   const password = req.body.password;
 
-  User.findOne({email: email}, function(err, foundUser){
-      if (err) {
-        console.log(err);
-      } else {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
-      }
-  });
-
-});
-
-app.get("/register", function(req, res){
-  res.render("register");
-});
-
-app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-  newUser.save(function(err){
+  User.findOne({
+    email: email
+  }, function(err, foundUser) {
     if (err) {
       console.log(err);
     } else {
-      res.render("secrets");
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+              res.render("secrets");
+          }
+        });
+      }
     }
   });
+
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+app.post("/register", function(req, res) {
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  });
+
+
 });
 
 
